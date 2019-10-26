3eb4b2d7324e8fc28a066bf5b04874d736caa252
Kaori Oka <kaorioka09jm@gmail.com>
2019-10-27 00:39:04 +0900

    try_controller_test

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 describe TweetsController, type: :controller do
 
-  describe 'GET #new' do
-    it "renders the :new template" do
-      get :new
+  describe 'GET #index' do
+    it "populates an array of tweets ordered by created_at DESC" do
+      tweets = create_list(:tweet, 3)
+      get :index
+      expect(assigns(:tweets)).to match(tweets.sort{|a, b| b.created_at <=> a.created_at })
+    end
 
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
     end
   end
 
