3def11de892e27d3556cdc43f293a53717aa098c
Kaori Oka <kaorioka09jm@gmail.com>
2019-10-27 00:30:52 +0900

    trya_new_action2

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
-describe TweetsController do
+describe TweetsController, type: :controller do
+
   describe 'GET #new' do
     it "renders the :new template" do
+      get :new
+
     end
   end
 
